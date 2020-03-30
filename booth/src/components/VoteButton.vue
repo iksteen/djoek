3bfe061d292505bd71915eb9228c9cf9bbe1c6de
Ingmar Steen <iksteen@gmail.com>
2020-03-31 00:23:37 +0200

    Add missing VoteButton.vue.

---
+++ booth/src/components/VoteButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on }">
+      <v-btn
+        text
+        v-on="on"
+        @click.stop="$emit('click')"
+      >
+        <v-icon>
+          mdi-thumb-{{ direction }}
+        </v-icon>
+        <span class="ml-1">
+          ({{ votes }})
+        </span>
+      </v-btn>
+    </template>
+    <span>Vote {{ direction }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+  export default {
+    name: 'VoteButton',
+    props: {
+      votes: Number,
+      direction: String,
+    },
+  }
+</script>
